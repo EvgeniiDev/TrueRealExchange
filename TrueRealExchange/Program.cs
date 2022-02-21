21a984a0baa370ec31278080ded40da916aa248d
Сергеев Евгений <evgeniisergeev2002@list.ru>
2022-02-21 20:51:22 +0500

    some fix in progran.cs

--- TrueRealExchange/Program.cs
+++ TrueRealExchange/Program.cs
@@ -6,10 +6,10 @@ namespace TrueRealExchange
     {
         static void Main(string[] args)
         {
-            var virtualExchange = new Exchange((IExchange)new RealExchange());
-            var acc1 = virtualExchange.CreateAccount("Aboba", 10000);
-            var acc2 = virtualExchange.CreateAccount("Aboba", 10000);
-            var acc3 = virtualExchange.CreateAccount("Aboba", 10000);
+            var virtualExchange = new Exchange((IExchange) new RealExchange());
+            var acc1 = virtualExchange.CreateAccount("Aboba", "USD", 10000);
+            var acc2 = virtualExchange.CreateAccount("Aboba", "USD", 10000);
+            var acc3 = virtualExchange.CreateAccount("Aboba", "USD", 10000);
 
             Console.WriteLine("Hello World!");
         }
